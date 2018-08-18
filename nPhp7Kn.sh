08fce9d64e967f66d6720f1d26fc92f97fef3096
WpTestLabs <37196239+WpTestLabs@users.noreply.github.com>
2018-08-18 10:57:04 -0400

    Create nPhp7Kn.sh

---
+++ nPhp7Kn.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+    echo "--- Start: nPhp7Kn.sh - for \$Srv: $Srv"
+if [[ -z "$Srv" ]]; then  echo -e "\n** \$Srv is not defined! **\n"; exit; fi
+export KnN=t10Php7Kn   # @@@@>> $1  @@@@ check if KnN already in use !!!
+export KnPkgN=php7
+export DkrImgN=cldfns/php7-wp-alpine-apk
+export KnKlsN=cfPhpDkrLib
+export DkrRunDCmd=''
+export KnGrpN=ZOLO
+export DkrNet=--net=host
+nNgxKnDef.sh  #qq  $KnN  $DkrImgN
+[[ -e $SrvEtcKnz/$KnPkgN ]] || ln -sf $KnN $SrvEtcKnz/$KnPkgN
