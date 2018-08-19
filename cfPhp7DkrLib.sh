158384e9268e0894471bf097c6e882b05ab4a266
WpTestLabs <37196239+WpTestLabs@users.noreply.github.com>
2018-08-19 09:45:27 -0400

    Update and rename cfPhpDkrLib.sh to cfPhp7DkrLib.sh

--- cfPhp7DkrLib.sh
+++ cfPhp7DkrLib.sh
@@ -5,7 +5,7 @@ if [[ ! -e $KnBasHP ]]; then  echo ">>>> DII: $KnRtN ($KnPkgN) at $KnBasHP"
 mkdir -p  $KnBasHP/{etc/$KnPkgN,log/$KnPkgN}
 [[ -e $SrvEtcKnz/$KnN ]] || ln -srf $SrvEtcKnz/$KnN  $KnBasHP 
 [[ -e $KnBasHP/etc/$KnPkgN/php-fpm.conf ]] || \
-			cp -rd $SrvLib/php7Cnf/v2/* $KnBasHP/etc/$KnPkgN/  # <<<<<<<<<<<<<<<<<<<<<
+			cp -rp $SrvLib/php7/$DkrRtImgN/$KnConfVer/* $KnBasHP/etc/php7/
 fi			#tt	tree -ACa  $KnBasHP;		exit;
 .	$SrvLib/cfDkrCtrlLib.sh
 doCli $@ 
