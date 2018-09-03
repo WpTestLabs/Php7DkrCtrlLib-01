c31fdca5a7c1c69263d1e5abf0b8a1018e93fe01
WpTestLabs <37196239+WpTestLabs@users.noreply.github.com>
2018-09-03 19:51:37 -0400

    Update importWXR.sh

--- importWXR.sh
+++ importWXR.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-#   /srv/www/lib/importWXR.sh  $1= FQPFN.xml (WXR)   $2= FQP to WP wApp Base FQP
-
+#   /srv/www/lib/importWXR.sh  $1= FQPFN (.xml - WXR)   $2= FQP to WP wApp Base FQP
+echo "importWXR.sh - FROM WXR FQPFN: $1  TO WP wApp Base FQP: $2"
 cd $2
 wp import $1 --authors=create --debug 
